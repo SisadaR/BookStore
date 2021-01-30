6af68d2b9f9dacc1e8ba6c2819d38e2c0e576d75
Sisada R <kong.sisada@gmail.com>
2021-01-31 03:52:46 +0700

    add stagged layout

--- app/src/main/java/com/sisada/bookstore/FragmentThree.kt
+++ app/src/main/java/com/sisada/bookstore/FragmentThree.kt
@@ -5,11 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 
 
 class FragmentThree : Fragment() {
 
-
+    private  var fictionList : MutableList<Int> = mutableListOf(
+        R.drawable.fiction_one,R.drawable.fiction_six,
+        R.drawable.fiction_two,R.drawable.fiction_seven,
+        R.drawable.fiction_three,R.drawable.fiction_eight,
+        R.drawable.fiction_four,R.drawable.fiction_nine,
+        R.drawable.fiction_five,R.drawable.fiction_ten,
+        R.drawable.fiction_one,R.drawable.fiction_six,
+        R.drawable.fiction_two,R.drawable.fiction_seven,
+        R.drawable.fiction_three,R.drawable.fiction_eight,
+        R.drawable.fiction_four,R.drawable.fiction_nine,
+        R.drawable.fiction_five,R.drawable.fiction_ten,
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -21,7 +35,10 @@ class FragmentThree : Fragment() {
     ): View? {
 
         val view = inflater.inflate(R.layout.fragment_three, container, false)
-
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
+        val layout = StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL)
+        recyclerView.layoutManager = layout
+        recyclerView.adapter = RecyclerViewAdapter(fictionList)
         return view
     }
 
--- app/src/main/java/com/sisada/bookstore/FragmentTwo.kt
+++ app/src/main/java/com/sisada/bookstore/FragmentTwo.kt
@@ -5,10 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 
 class FragmentTwo : Fragment() {
 
+    private  var childrenList : MutableList<Int> = mutableListOf(
+        R.drawable.children_one,R.drawable.children_six,
+        R.drawable.children_two,R.drawable.children_seven,
+        R.drawable.children_three,R.drawable.children_eight,
+        R.drawable.children_four,R.drawable.children_nine,
+        R.drawable.children_five,R.drawable.children_ten,
+        R.drawable.children_one,R.drawable.children_six,
+        R.drawable.children_two,R.drawable.children_seven,
+        R.drawable.children_three,R.drawable.children_eight,
+        R.drawable.children_four,R.drawable.children_nine,
+        R.drawable.children_five,R.drawable.children_ten,
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -20,6 +34,10 @@ class FragmentTwo : Fragment() {
     ): View? {
 
         val view = inflater.inflate(R.layout.fragment_two, container, false)
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
+        val layout = GridLayoutManager(context!!,3)
+        recyclerView.layoutManager = layout
+        recyclerView.adapter = RecyclerViewAdapter(childrenList)
 
         return view
     }
